1be71f80106e509d50d6dab4a8f2e39f2eaa7a31
Edward Danilyuk <edward.danilyuk@gmail.com>
2016-04-29 14:44:15 -0700

    Add procedure add_page

--- create-index.py
+++ create-index.py
@@ -12,3 +12,8 @@ def lookup(index, word):
       if entry[0] == keyword:
         return entry[1]
     return []
+
+def add_page_to_index(index, url, content):
+  words = content.split()
+  for word in words:
+    add_to_index(index, word, url)
