d72f3c38637aa4b105bbd4aa9f5787874fe818ff
Edward Danilyuk <edwardvdanilyuk@gmail.com>
2016-05-29 23:35:37 -0700

    Update create-index.py

--- create-index.py
+++ create-index.py
@@ -3,7 +3,7 @@ index = []
 def add_to_index(index, keyword, url):
   for entry in index:
     if entry[0] == keyword:
-      if not url in entry[1]
+      if not url in entry[1]:
         entry[1].append(url)
       return
   index.append([keyword, [url]])
